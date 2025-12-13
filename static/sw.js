3868cae0f4fb64bcdeb381d16811a7f96bff3562
Bekiboo <julien.connault@gmail.com>
2025-12-13 17:08:21 +0300

    Refactor service worker registration and caching strategy; update to version 2 and streamline asset management

--- static/sw.js
+++ static/sw.js
@@ -1,44 +1,39 @@
-const CACHE_NAME = 'calorix-v1';
-const urlsToCache = [
-	'/',
-	'/setup',
-	'/history',
-	'/stats'
+const CACHE_NAME = 'calorix-v2';
+
+const STATIC_ASSETS = [
+  '/',
+  '/manifest.json'
 ];
 
-// Install service worker
+// Install
 self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then((cache) => cache.addAll(urlsToCache))
-	);
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
 });
 
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches.match(event.request)
-			.then((response) => {
-				// Cache hit - return response
-				if (response) {
-					return response;
-				}
-				return fetch(event.request);
-			})
-	);
-});
-
-// Activate service worker and remove old caches
+// Activate
 self.addEventListener('activate', (event) => {
-	event.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cacheName) => {
-					if (cacheName !== CACHE_NAME) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        )
+      )
+    ])
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
 });
